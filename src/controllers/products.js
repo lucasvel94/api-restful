0661cafb43f77c94158b4a5dffef2794f008446b
lucas <lucas.vel94@gmail.com>
2021-12-03 14:35:23 -0300

    adicionando metodo PUT

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -35,8 +35,33 @@ async function post(req, res) {
     })
 }
 
+async function put(req, res) {
+ const { id } = req.params
+
+ const product = await ProductsModel.findOneAndUpdate({ _id: id }, req.body, { new: true })
+
+ res.send({
+     message: 'success',
+     product,
+ })
+
+
+ /*  NESTE METODO NAO RETORNA O PRODUTO ATUALIZADO
+ const product = await ProductsModel.findOne({ _id: id })
+
+ await product.updateOne(req.body)
+
+ res.send({
+    message: 'sucess',
+    product,
+}) 
+*/
+
+}
+
 
 module.exports = {
     get,
     post,
+    put,
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -13,7 +13,7 @@ const ProductController = require('../controllers/products')
 
 router.get('/products/:id?', ProductController.get)
 router.post('/products', ProductController.post)
-//router.put('/products', ProductController.put)
+router.put('/products/:id', ProductController.put)
 //router.delete('/products', ProductController.delete)
 
 
